223e8ae23058e4e4b3b4fb18115c3b5ac253455d
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-05 17:14:52 -0600

    Add hacky logfile so I can see what's going on.

--- pathchecker.py
+++ pathchecker.py
@@ -3,14 +3,16 @@
 
 '''Path check plugin based on spell check plugin.'''
 
+import sys
 from zim.plugins import PluginClass
 
 class PathChecker(PluginClass):
 
 	plugin_info = {
-		'name': _('Path Checker'),
-		'description': _('''\
-Adds path checking.
-'''),
-		'author': 'Nathaniel Beaver',
+		'name': _("Path Checker"), # T: plugin name
+		'description': _("Checks for broken paths in links."), # T: plugin description
+		'author': "Nathaniel Beaver",
 	}
+
+with open('/tmp/pathchecker.log', 'a') as mylog:
+	mylog.write('running from '+sys.argv[0]+'\n')
