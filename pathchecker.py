e952019484c4ed1883ff3f6fb8d4a5a66debd928
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-05 17:16:18 -0600

    Took some stuff from the zeitgeist plugin.

--- pathchecker.py
+++ pathchecker.py
@@ -4,7 +4,11 @@
 '''Path check plugin based on spell check plugin.'''
 
 import sys
-from zim.plugins import PluginClass
+from zim.plugins import PluginClass, ObjectExtension, extends
+from zim.signals import SIGNAL_AFTER
+import logging
+
+logger = logging.getLogger('zim.plugins.pathcheck')
 
 class PathChecker(PluginClass):
 
@@ -16,3 +20,28 @@ class PathChecker(PluginClass):
 
 with open('/tmp/pathchecker.log', 'a') as mylog:
 	mylog.write('running from '+sys.argv[0]+'\n')
+
+@extends('PageView')
+class PageViewExtension(ObjectExtension):
+
+	def __init__(self, plugin, pageview):
+		self.plugin = plugin
+		self.connectto_all(pageview.ui, ('open-page'), order=SIGNAL_AFTER)
+
+	def on_open_page(self, ui, page, path):
+		with open('/tmp/pathchecker.log', 'a') as mylog:
+			mylog.write(page.name + '\n')
+
+@extends('Notebook')
+class NotebookExtension(ObjectExtension):
+
+	def __init__(self, plugin, notebook):
+		self.plugin = plugin
+		self.connectto_all(notebook,
+			('deleted-page', 'stored-page'), order=SIGNAL_AFTER)
+
+	def on_deleted_page(self, page, path):
+		logger.debug("Deleted page: %s", page.name)
+
+	def on_stored_page(self, page, path):
+		logger.debug("Modified page: %s", page.name)
